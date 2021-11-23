2ae189d1a728d7ed06c297f2966f4d8554e413ab
Saniya <saniya.fmg@gmail.com>
2021-11-23 09:49:20 +0600

    Auto-generated commit [ci skip]

--- assets/env.js
+++ assets/env.js
@@ -3,6 +3,7 @@
 
   // Environment variables
   window["env"]["baseUrl"] = "https://business-dev.jysanbank.kz:8460/api/v1";
+  // window["env"]["baseUrl"] = "https://business-staging.jysanbank.kz:8460/api/v1";
   // window["env"]["baseUrl"] = "https://business-test1.jysanbank.kz:8460/api/v1";
   window["env"]["appVersion"] = "";
   window["env"]["gaTrackingId"] = "";
