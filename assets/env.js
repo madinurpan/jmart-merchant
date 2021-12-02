2f273ba10b977dce33aedbf51dd62559664a6d8c
madinurpan <madinurpanov@gmail.com>
2021-12-02 22:56:35 +0600

    Auto-generated commit [ci skip]

--- assets/env.js
+++ assets/env.js
@@ -2,8 +2,10 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
+  // window["env"]["baseUrl"] = "https://business-uat.jysanbank.kz:8460/api/v1";
   window["env"]["baseUrl"] = "https://business-dev.jysanbank.kz:8460/api/v1";
+  // window["env"]["baseUrl"] = "https://business-staging.jysanbank.kz:8460/api/v1";
+  // window["env"]["baseUrl"] = "https://business-test1.jysanbank.kz:8460/api/v1";
   window["env"]["appVersion"] = "";
-  window["env"]["gaTrackingId"] = 'G-SJK4DXEWFR';
-  window["env"]["AmplitudeApiKey"] = 'e74d47f441f931f8d6a2875b8af3d6ab';
+  window["env"]["gaTrackingId"] = "";
 })(this);
--- assets/env.template.js
+++ assets/env.template.js
@@ -5,5 +5,4 @@
   window["env"]["baseUrl"] = "${BASE_URL}";
   window["env"]["appVersion"] = "${APP_VERSION}";
   window["env"]["gaTrackingId"] = "${GA_TRACKING_ID}";
-  window["env"]["AmplitudeApiKey"] = "${AMPLITUDE_API_KEY}";
 })(this);
