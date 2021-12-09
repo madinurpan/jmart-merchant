fce1ec13bf61917ce8d9f237199e7ff09d9a34d7
madinurpan <madinurpanov@gmail.com>
2021-12-10 04:29:56 +0600

    Auto-generated commit [ci skip]

--- assets/env.template.js
+++ assets/env.template.js
@@ -5,4 +5,5 @@
   window["env"]["baseUrl"] = "${BASE_URL}";
   window["env"]["appVersion"] = "${APP_VERSION}";
   window["env"]["gaTrackingId"] = "${GA_TRACKING_ID}";
+  window["env"]["AmplitudeApiKey"] = "${AMPLITUDE_API_KEY}";
 })(this);
