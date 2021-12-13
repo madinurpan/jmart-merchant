de7ced419da4bd3cad8120b95591f4cecfbba702
Madiyar <55392433+iznalfiden@users.noreply.github.com>
2021-12-13 15:49:42 +0600

    Auto-generated commit [ci skip]

--- assets/env.js
+++ assets/env.js
@@ -2,7 +2,7 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["baseUrl"] = "https://business-staging.jysanbank.kz:8460/api/v1";
+  window["env"]["baseUrl"] = "https://business-dev.jysanbank.kz:8460/api/v1";
   window["env"]["appVersion"] = "";
   window["env"]["gaTrackingId"] = 'G-SJK4DXEWFR';
   window["env"]["AmplitudeApiKey"] = 'e74d47f441f931f8d6a2875b8af3d6ab';
